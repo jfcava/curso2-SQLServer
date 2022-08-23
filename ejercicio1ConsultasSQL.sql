202f02135043712b4745935413449ef32dadcce5
jfcava <juancavallieri@gmail.com>
2022-08-23 01:05:38 -0300

    C# - SQL SERVER - Guia de Ejercicios 1

--- ejercicio1ConsultasSQL.sql
+++ ejercicio1ConsultasSQL.sql
@@ -6,8 +6,8 @@
 -- 1- Traer todos los discos con su respectivo estilo. 
 --	  La consulta debe mostrar: Nombre de disco, Fecha de lanzamiento, Estilo (no el id, sino la descripción).
 
--- Select D.Titulo, D.FechaLanzamiento, E.Descripcion Estilo, T.Descripcion Formato From DISCOS D, ESTILOS E, TIPOSEDICION T 
--- where D.IdEstilo = E.Id AND D.IdTipoEdicion = T.Id
+-- Select D.Titulo, D.FechaLanzamiento, E.Descripcion Estilo From DISCOS D, ESTILOS E 
+-- where D.IdEstilo = E.Id
 
 
 
@@ -21,6 +21,7 @@
 -- 3- Insertar al menos dos discos nuevos cargando correctamente su información.
 -- insert into DISCOS values ('El amor despues del amor', '1998/02/24', 14, '', 4, 4)
 -- insert into DISCOS values ('El polaco y yo', getdate(), 10, '', 6, 4)
+-- insert into DISCOS values ('Nevermind', getdate(), 12, '', 4, 4) 
 
 -- Modifico la instruccion anterior porque le puse mal el Id.
 -- update DISCOS set IdEstilo = 5 where Id = 3
@@ -35,7 +36,10 @@
 -- delete From DISCOS where Id = 3
 
 -- 6- Traer todos los estilos que estén asociados a algún disco.
-select E.Descripcion FROM DISCOS D, ESTILOS E where D.Id > 0
+-- select E.Descripcion Estilo from DISCOS D, ESTILOS E where IdEstilo > 0 AND D.IdEstilo = E.Id
 
+-- 7- Traer todos los discos con el siguiente formato: Nombre, Estilo, Edición (todo texto).
+-- select D.Titulo Nombre, E.Descripcion Estilo, T.Descripcion Edicion From DISCOS D, ESTILOS E, TIPOSEDICION T where D.IdEstilo = E.Id AND D.IdTipoEdicion = T.Id
 
-
+-- 8- Traer todos los discos que contengan en su nombre la sílaba "ho".
+-- select Titulo From DISCOS D where D.Titulo LIKE '%ho%'
